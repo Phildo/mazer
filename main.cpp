2d8ce71e8ce53263fb1187a591c48f205cb80310
Phildo <Phildo211@gmail.com>
2013-08-24 18:12:42 -0500

    starting use of events

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <SDL/SDL.h>
-#include <SDL/SDL_image.h> //"/Library/Frameworks/SDL2_image.framework/Versions/A/Headers/SDL_image.h"
+#include <SDL/SDL_image.h>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP = 32; //bits-per-pixel
+
+SDL_Event event;
+SDL_Surface *hello = NULL;
+SDL_Surface *screen = NULL;
 
 SDL_Surface *load_image(char *filename)
 {
@@ -28,21 +32,45 @@ void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination)
   SDL_BlitSurface(source, NULL, destination, &offset);
 }
 
-int main(int argc, char **args)
+bool init()
 {
-  SDL_Surface *hello = NULL;
-  SDL_Surface *screen = NULL;
   if(SDL_Init(SDL_INIT_EVERYTHING) == -1) return 1;
-  SDL_WM_SetCaption("mazer",NULL);
   screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
   if(screen == NULL) return 1;
+  SDL_WM_SetCaption("mazer",NULL);
+  return true;
+}
+
+bool load_files()
+{
   hello = load_image("assets/proud_face.png");
-  apply_surface(100,100,hello,screen);
-  if(SDL_Flip(screen) == -1) return 1;
-  SDL_Delay(2000);
+  if(hello == NULL) return false;
+  return true;
+}
+
+void clean_up()
+{
   SDL_FreeSurface(hello);
   SDL_FreeSurface(screen);
   SDL_Quit();
+}
+
+int main(int argc, char **args)
+{
+  if(!init())       return 1;
+  if(!load_files()) return 1;
+
+  bool quit = false;
+
+  apply_surface(100,100,hello,screen);
+  if(SDL_Flip(screen) == -1) return 1;
+  
+  while(!quit)
+  {
+    if(SDL_PollEvent(&event) && event.type == SDL_QUIT) quit = true;
+  }
+
+  clean_up();
   return 0;
 }
 
