3865f1ef06f1cc6db41d2f76f5c90d60d7a87da8
Phildo <Phildo211@gmail.com>
2013-08-31 17:38:15 -0500

    finally sensible generation! (could be made faster using hashmaps, but this should be sufficient)

--- src/maze.cpp
+++ src/maze.cpp
@@ -36,45 +36,77 @@ void openBlockWall(MazeBlock *blocks, int x, int y, int width, int height, MazeB
   if(t & WALL_LEFT)   blocks[(width* y)   +x-1].type = (MazeBlockWallType)(blocks[(width* y)   +x-1].type & ~WALL_RIGHT);
 }
 
+bool hasAvailableEdges(MazeBlock *blocks, int x, int y, int width, int height)
+{
+    if(blocks[((y+1)*width)+x].type == WALL_ALL) return true;
+    if(blocks[((y-1)*width)+x].type == WALL_ALL) return true;
+    if(blocks[(y*width)+(x+1)].type == WALL_ALL) return true;
+    if(blocks[(y*width)+(x-1)].type == WALL_ALL) return true;
+
+    return false;
+}
+
 MazeBlock *generate(int width, int height)
 {
   MazeBlock *blocks = new MazeBlock[width*height];
-  std::list<MazeBlock> curWalk;
+  MazeBlock *curBlock;
   int x;
   int y;
-  int newx;
-  int newy;
-  MazeBlockWallType dir;
 
   //set default state
   for(y = 0; y < height; y++)
     for(x = 0; x < width; x++)
+    {
       blocks[(y*width)+x].type = WALL_NONE;
+      blocks[(y*width)+x].x = x;
+      blocks[(y*width)+x].y = y;
+    }
   for(y = 1; y < height-1; y++)
     for(x = 1; x < width-1; x++)
       closeBlockWall(blocks,x,y,width,height,WALL_ALL);
 
   //drunken walk through for a bit
-  x = 1;
-  y = 1;
-  for(int i = 0; i < 500; i++)
-  {
-    newx = x;
-    newy = y;
-    switch(rand()%4)
-    {
-      case 0: dir = WALL_BOTTOM; newy = (y+1); break;
-      case 1: dir = WALL_LEFT;   newx = (x-1); break;
-      case 2: dir = WALL_TOP;    newy = (y-1); break;
-      case 3: dir = WALL_RIGHT;  newx = (x+1); break;
-    }
 
-    if(newx != 0 && newx != width-1 && newy != 0 && newy != height-1)
+  std::list<MazeBlock *> edges;
+  std::list<MazeBlock *>::iterator e_it;
+  edges.push_back(&blocks[(1*width)+1]);
+
+  int newx;
+  int newy;
+  MazeBlockWallType dir;
+  while(!edges.empty())
+  {
+    curBlock = *edges.begin();
+    x = curBlock->x;
+    y = curBlock->y;
+
+    while(hasAvailableEdges(blocks, x, y, width, height))
     {
-      if(blocks[(newy*width)+newx].type == WALL_ALL)// || (~(int)(blocks[(y*width)+x].type) & dir))
+      newx = x;
+      newy = y;
+      switch(rand()%4)
+      {
+        case 0: dir = WALL_BOTTOM; newy = (y+1); break;
+        case 1: dir = WALL_LEFT;   newx = (x-1); break;
+        case 2: dir = WALL_TOP;    newy = (y-1); break;
+        case 3: dir = WALL_RIGHT;  newx = (x+1); break;
+      }
+
+      if(newx != 0 && newx != width-1 && newy != 0 && newy != height-1 && blocks[(newy*width)+newx].type == WALL_ALL)// || (~(int)(blocks[(y*width)+x].type) & dir))
+      {
         openBlockWall(blocks,x,y,width,height,dir);
-      x = newx;
-      y = newy;
+        if(!hasAvailableEdges(blocks, x, y, width, height)) edges.remove(curBlock);
+        x = newx;
+        y = newy;
+        curBlock = &blocks[(y*width)+x];
+        if(hasAvailableEdges(blocks, x, y, width, height)) edges.push_back(curBlock);
+
+        //expensive...
+        if(!hasAvailableEdges(blocks, x, (y+1), width, height)) edges.remove(&blocks[((y+1)*width)+x]);
+        if(!hasAvailableEdges(blocks, x, (y-1), width, height)) edges.remove(&blocks[((y-1)*width)+x]);
+        if(!hasAvailableEdges(blocks, (x+1), y, width, height)) edges.remove(&blocks[(y*width)+(x+1)]);
+        if(!hasAvailableEdges(blocks, (x-1), y, width, height)) edges.remove(&blocks[(y*width)+(x-1)]);
+      }
     }
   }
   return blocks;
