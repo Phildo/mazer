305554cf01e936225854736a9e5ddb2d62a38534
Phildo <Phildo211@gmail.com>
2013-08-31 14:33:58 -0500

    naive maze generation functional

--- src/maze.cpp
+++ src/maze.cpp
@@ -38,48 +38,43 @@ void openBlockWall(MazeBlock *blocks, int x, int y, int width, int height, MazeB
 
 MazeBlock *generate(int width, int height)
 {
-  int x = 0;
-  int y = 0;
   MazeBlock *blocks = new MazeBlock[width*height];
+  std::list<MazeBlock> curWalk;
+  int x;
+  int y;
+  int newx;
+  int newy;
+  MazeBlockWallType dir;
 
-  //set default full-maze state
+  //set default state
   for(y = 0; y < height; y++)
     for(x = 0; x < width; x++)
       blocks[(y*width)+x].type = WALL_NONE;
-
   for(y = 1; y < height-1; y++)
     for(x = 1; x < width-1; x++)
       closeBlockWall(blocks,x,y,width,height,WALL_ALL);
 
   //drunken walk through for a bit
-
-  //std::list<MazeBlock> curWalk;
-  MazeBlockWallType dir;
-  int newx;
-  int newy;
-  for(int e = 0; e < 15; e++)
+  x = 1;
+  y = 1;
+  for(int i = 0; i < 500; i++)
   {
-    x = (rand()%37)+1;//0-38;
-    y = (rand()%27)+1;//0-28;
-    for(int i = 0; i < 50; i++)
+    newx = x;
+    newy = y;
+    switch(rand()%4)
     {
-      newx = x;
-      newy = y;
-      switch(rand()%4)
-      {
-        case 0: dir = WALL_BOTTOM; newy = (y+1); break;
-        case 1: dir = WALL_LEFT;   newx = (x-1); break;
-        case 2: dir = WALL_TOP;    newy = (y-1); break;
-        case 3: dir = WALL_RIGHT;  newx = (x+1); break;
-      }
+      case 0: dir = WALL_BOTTOM; newy = (y+1); break;
+      case 1: dir = WALL_LEFT;   newx = (x-1); break;
+      case 2: dir = WALL_TOP;    newy = (y-1); break;
+      case 3: dir = WALL_RIGHT;  newx = (x+1); break;
+    }
 
-      if(blocks[(newy*width)+newx].type == WALL_ALL || (~(int)(blocks[(y*width)+x].type) & dir))
-      {
+    if(newx != 0 && newx != width-1 && newy != 0 && newy != height-1)
+    {
+      if(blocks[(newy*width)+newx].type == WALL_ALL)// || (~(int)(blocks[(y*width)+x].type) & dir))
         openBlockWall(blocks,x,y,width,height,dir);
-        x = newx;
-        y = newy;
-        //curWalk.push_front(blocks[(newy*width)+newx]);
-      }
+      x = newx;
+      y = newy;
     }
   }
   return blocks;
